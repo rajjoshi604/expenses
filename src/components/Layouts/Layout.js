091a41b83cd0454932211ef3feb9e94698632837
rajjoshi604 <rajtest527@gmail.com>
2025-05-27 23:42:01 +0530

    Add files via upload

---
+++ src/components/Layouts/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './header'
+import Footer from './footer'
+
+const Layout = ( {children}) => {
+     return (
+<>
+  < Header />
+  <div className='content'>{children} </div>
+  <Footer/>
+</>
+
+     );
+
+
+};
+
+export default Layout;
